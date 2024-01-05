7008b8ba6d7fbeec91cc8213a0fde5910a6b6c59
mobinkaarimi <68745874+mobinkaarimi@users.noreply.github.com>
2024-01-05 18:44:45 +0330

    update[(routes)]-> create first and initial page

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className={`container mx-auto my-16 px-5 sm:px-1 md:px-0`}>
+      <h1 className="text-white">Hello world</h1>
+    </div>
+  );
+}
