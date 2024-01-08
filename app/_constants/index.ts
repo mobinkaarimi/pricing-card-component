ac0eb23b29cf0669d681f70c7747e82c0dd8f1ab
mobinkaarimi <68745874+mobinkaarimi@users.noreply.github.com>
2024-01-08 23:50:02 +0330

    update[index.ts]-> create mock data Object for plans of price list

---
+++ app/_constants/index.ts
@@ -0,0 +1,24 @@
+export const pricePlanList = [
+  {
+    id: 1,
+    title: 'Starter',
+    price: 19,
+    features: ["500 MAUs","3 projects","Unlimited guides","Unlimited flows","Unlimited branded themes","Email support"],
+    buttonText: "Choose Plan"
+  },
+  {
+    id: 2,
+    title: 'Pro',
+    price: 99,
+    features: ["All starter features, plus:", "Unlimited projects","Unlimited fully customizable themes","A dedicated Customer Success Manager"],
+    buttonText: "Choose Plan"
+  },
+  {
+    id: 3,
+    title: 'Enterprise',
+    price: "Let's Talk!",
+    features: ["All Pro features", "Unlimited MAUs","Dedicated environment","Enterprise account administration","Premium support and services"],
+    buttonText: "Contact US"
+  },
+];
+  
