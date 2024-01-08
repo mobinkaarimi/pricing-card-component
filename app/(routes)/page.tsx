a86dbad2d37409debe442515941ca182547c5730
mobinkaarimi <68745874+mobinkaarimi@users.noreply.github.com>
2024-01-08 23:52:13 +0330

    update[page.tsx]-> update Home page update default ui and add fixed box of top

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,13 @@
+// Styles
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={`container mx-auto my-16 px-5 sm:px-1 md:px-0`}>
-      <h1 className="text-white">Hello world</h1>
+    <div className={`${styles.layout} container mx-auto my-16 px-5 sm:px-1 md:px-0`}>
+      <div className="absolute top-0 left-2/4 py-3 px-6 bg-[#BC1E4A]">
+        <p className="text-sm">Your current plan:</p>
+        <p>Starter Trial . 500MAUs</p>
+      </div>
     </div>
   );
 }
