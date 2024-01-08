cbe43ccc78c313cc361b529c56743b970b981e6b
mobinkaarimi <68745874+mobinkaarimi@users.noreply.github.com>
2024-01-08 23:54:39 +0330

    create[index.tsx]-> create PriceCardList component

---
+++ app/_components/PriceCardList/index.tsx
@@ -0,0 +1,9 @@
+
+export default function PriceCardList() {
+  
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-8">
+      {/* Price List Here */}
+    </div>
+  );
+}
